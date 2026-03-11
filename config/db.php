463118541805355da35fa21767f7c92d280bd7ea
hiepquocchung5-blip <hiepquocchung5@gmail.com>
2026-03-11 14:33:39 +0630

    psw update

--- config/db.php
+++ config/db.php
@@ -10,7 +10,7 @@
 $host = 'localhost';
 $db   = 'shinedana_co';
 $user = 'shinedana_co_usr';      // Default XAMPP user
-$pass = 'Mk1=>A{yek`2[>S7';          // Default XAMPP password (leave empty)
+$pass = 'Fk_q./OX}c_cn.!';          // Default XAMPP password (leave empty)
 $charset = 'utf8mb4';
 
 $dsn = "mysql:host=$host;dbname=$db;charset=$charset";
