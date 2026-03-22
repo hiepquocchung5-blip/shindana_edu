cb3d7c8d490180dda365b377a48b7ceb17bda1a6
hiepquocchung5-blip <hiepquocchung5@gmail.com>
2026-03-22 10:23:54 +0630

    defer add on ass alpine

--- auth/login.php
+++ auth/login.php
@@ -129,7 +129,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST['login_btn'])) {
     
     <!-- Tailwind & Alpine.js -->
     <script src="https://cdn.tailwindcss.com"></script>
-    <script src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
+    <script defer src="https://unpkg.com/alpinejs@3.x.x/dist/cdn.min.js"></script>
     
     <!-- Fonts & Icons -->
     <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@300;400;600;700;800&display=swap" rel="stylesheet">
